f5936aef5db02c3d1b17aba6e6e87d538d2d122f
Satya Mohapatra <mohapsat@gmail.com>
2018-11-14 18:25:54 -0800

    Removed path for STATICFILES_DIRS. Getting an error while creating superuser ?: (staticfiles.E002) The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting.

--- tumidpandora_school_rewards/tumidpandora_school_rewards/settings.py
+++ tumidpandora_school_rewards/tumidpandora_school_rewards/settings.py
@@ -140,9 +140,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#  STATICFILES_DIRS is the list of folders where Django will search for additional static files
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+STATICFILES_DIRS = []
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
